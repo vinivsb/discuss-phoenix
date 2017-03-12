1e9c47e2da570c7bcf85e5c7c6f6561cc379686d
cs-vinicius-barbosa <vinicius.barbosa@concretesolutions.com.br>
2017-03-12 01:03:18 -0300

    Adding phoenix CRUD topics forum and docker-compose

---
+++ web/models/topic.ex
@@ -0,0 +1,17 @@
+defmodule Discuss.Topic do
+  use Discuss.Web, :model
+
+  schema "topics" do
+    field :title, :string
+    belongs_to :user, Discuss.User
+    has_many :topics, Discuss.Topic
+    has_many :comments, Discuss.Comment
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
+
+end
